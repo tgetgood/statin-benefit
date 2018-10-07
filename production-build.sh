89951a25319aad9be25813cfc55a96e982f56ea6
Thomas Getgood <thomas.getgood@gmail.com>
2018-10-06 20:27:59 -0400

    More sensible place to put prod assets.

--- production-build.sh
+++ production-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cat resources/public/css/normalize.css resources/public/css/skeleton.css resources/public/css/app.css > build/main.css
+test -d resources/production || mkdir resources/production
 
-clojure -m cljs.main --optimizations advanced --output-to "build/main.js" -c statin-benefit.core
+cat resources/public/css/normalize.css resources/public/css/skeleton.css resources/public/css/app.css > resources/production/main.css
+
+clojure -m cljs.main --optimizations advanced --output-to "resources/production/main.js" -c statin-benefit.core
