b6ab97e488d74d369564e3cfc7b1e8bdd50b47bd
Thomas Getgood <thomas.getgood@gmail.com>
2018-10-11 16:23:56 -0400

    Moving production assets to public.
    This makes testing easier but has no other impact that I'm aware of.

--- production-build.sh
+++ production-build.sh
@@ -2,6 +2,6 @@
 
 test -d resources/production || mkdir resources/production
 
-cat resources/public/css/normalize.css resources/public/css/skeleton.css resources/public/css/app.css > resources/production/main.css
+cat resources/public/css/normalize.css resources/public/css/skeleton.css resources/public/css/app.css > resources/public/production/main.css
 
-clojure -m cljs.main --optimizations advanced --output-to "resources/production/main.js" -c statin-benefit.core
+clojure -m cljs.main --optimizations advanced --output-to "resources/public/production/main.js" -c statin-benefit.core
