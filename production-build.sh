ea375b085efc87a5397e7bf5e0182c0edb5ceef8
Thomas Getgood <thomas.getgood@gmail.com>
2018-10-30 13:04:54 -0400

    Don't need the linked dir any longer.

--- production-build.sh
+++ production-build.sh
@@ -2,10 +2,6 @@
 
 test -d resources/public/production || mkdir resources/public/production
 
-pushd resources
-test -L production || ln -s public/production production
-popd
-
 cat resources/public/css/normalize.css resources/public/css/skeleton.css resources/public/css/app.css > resources/public/production/main.css
 
 clojure -m cljs.main --optimizations advanced --output-to "resources/public/production/main.js" -c statin-benefit.core
